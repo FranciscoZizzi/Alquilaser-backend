1480349255c683e538e773a5e5d8029aeb74ccac
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-03-23 22:03:43 -0300

    implemented register functionality

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 router.get('/', function(req, res, next) {
     res.render('register');
-})
+});
+
+router.post('/', async function (req, res, next) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    if (!name || !email || !password) {
+        console.log("Name, email or password missing");
+        return res.status(400).send("Name, email or password missing");
+    }
+    if (password !== confirmPassword) {
+        console.log("Password does not match confirmed password");
+        return res.status(400).send("Password does not match confirmed password");
+    }
+    let existingUser = await User.findOne({where: {email: email}});
+    if (existingUser != null) {
+        console.log("Email already associated with an account");
+        return res.status(400).send("Email already associated with an account");
+    }
+    User.create({
+        name: name,
+        email: email,
+        password: password
+    });
+    console.log("created user " + name);
+    res.redirect('/');
+});
 
 module.exports = router;
