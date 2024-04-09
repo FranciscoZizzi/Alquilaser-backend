5d3f2069a2c189aeb1167b62c8bfd31f4c4b41c3
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-09 14:26:45 -0300

    search bar queries database and returns listings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const {searchService} = require("../services/searchService");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.post('/search', searchService);
 
 module.exports = router;
