47cec1fe2381e4db8254a2917a4c8e5c622524cc
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-15 20:32:37 -0300

    added token decoding

--- services/userService.js
+++ services/userService.js
@@ -102,3 +102,23 @@ exports.loginService = async (req, res) => {
         },
     });
 }
+
+exports.decodeToken = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token) {
+        res.status(200).json({
+            success: false,
+            message: "Token not provided"
+        });
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    res.status(200).json(
+        {
+            success: true,
+            data: {
+                userId: decodedToken.userId,
+                email: decodedToken.email
+            }
+        }
+    );
+}
