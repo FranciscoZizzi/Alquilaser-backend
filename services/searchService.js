3798d2e4f88cca4a4adbd6e2dcb8e92c9156f394
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-19 19:57:52 -0300

    implemented searchService

--- services/searchService.js
+++ services/searchService.js
@@ -2,87 +2,39 @@ const Listing = require('../models/listing');
 const {Op} = require("sequelize");
 
 exports.filteredSearch = async (req, res) => {
-    const priceFilterMin = req.body.priceFilterMin;
-    const priceFilterMax = req.body.priceFilterMax;
-    const searchTerm = req.body.searchTerm;
+    let priceFilterMin = req.body.min;
+    let priceFilterMax = req.body.max;
+    let searchTerm = req.body.searchTerm;
     let listings = []
-
-    if(!priceFilterMax && !priceFilterMin){
-        if(!searchTerm){
-            listings =  await Listing.findAll()
-            return res.send(listings)
-        }
-        listings = await Listing.findAll({where: {title: {[Op.like]: '%' + searchTerm + '%'}}})
-        return res.send(listings)
+    if (searchTerm == null) {
+        searchTerm = '';
     }
-    if(priceFilterMax && priceFilterMin){
-        if(!searchTerm){
-            listings = await Listing.findAll({
-                where: {
-                    price: {
-                        [Op.gte]: priceFilterMin,
-                        [Op.lte]: priceFilterMax,
-                    }
-                }
-            })
-            return res.send(listings)
-        }
+    // PriceFilterMin no puede ser nulo porque el front lo cambia a un 0 si lo es
+    if (priceFilterMax != null) {
         listings = await Listing.findAll({
             where: {
-                title: {[Op.like]: '%' + searchTerm + '%'},
+                title: {
+                    [Op.like]: '%' + searchTerm + '%'
+                },
                 price: {
                     [Op.gte]: priceFilterMin,
                     [Op.lte]: priceFilterMax,
                 }
             }
-        })
+        });
         return res.send(listings)
     }
-
-
-}
-
-exports.searchByTitle = async (req, res) => {
-    const priceFilterMin = req.body.priceFilterMin;
-    const priceFilterMax = req.body.priceFilterMax;
-    const searchTerm = req.body.searchTerm;
-    let listings
-    if(!priceFilterMax && !priceFilterMin){
-        if (!searchTerm) {
-            return res.status(400).send("womp womp")
-        }
-        listings = await Listing.findAll({where: {title: {[Op.like]: '%' + searchTerm + '%'}}});
-        console.log(listings)
-        return res.send(listings)
-    }
-    if(priceFilterMax || priceFilterMin){
-        listings = await searchByPrice(req,res)
-    }
-    return res.send(listings)
-}
-
-searchByPrice = async (req, res) => {
-    const searchTerm = req.body.searchTerm;
-    const priceFilterMin = req.body.priceFilterMin;
-    const priceFilterMax = req.body.priceFilterMax;
-    let listings = await Listing.findAll({
-        where: {
-            title: {[Op.like]: '%' + searchTerm + '%'},
-            price: {
-                [Op.gte]: priceFilterMin,
-                [Op.lte]: priceFilterMax,
+    if (priceFilterMax == null) {
+        listings = await Listing.findAll({
+            where: {
+                title: {
+                    [Op.like]: '%' + searchTerm + '%'
+                },
+                price: {
+                    [Op.gte]: priceFilterMin,
+                }
             }
-        }
-    })
-    return listings
-}
-
-exports.searchByListingState = async (req, res) => {
-    let listings = await Listing.findAll({
-        where: {
-            title: {[Op.like]: '%' + searchTerm + '%'},
-            listing_state: {[Op.like]:stateFilter}
-        }
-    })
-    return res.send(listings)
+        });
+        return res.send(listings)
+    }
 }
