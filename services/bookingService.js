41f7696fb450a23c30e272c6e6a3f0d1d1e9faf3
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-21 18:37:57 -0300

    makeBooking now gets userId from token

--- services/bookingService.js
+++ services/bookingService.js
@@ -4,7 +4,6 @@ const Booking = require('../models/booking');
 const {authenticationService} = require("./authenticationService");
 
 exports.makeBooking = async (req, res) => {
-    const userId = req.body.userId;
     const listingId = req.body.listingId;
     const startDate = req.body.startDate;
     const endDate = req.body.endDate;
@@ -13,6 +12,8 @@ exports.makeBooking = async (req, res) => {
     if (!authData.success) {
         return res.status(401).send("user not authenticated");
     }
+
+    const userId = authData.data.userId;
 
     let listing = await Listing.findByPk(listingId);
     if (!listing) {
