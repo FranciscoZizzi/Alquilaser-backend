81877ee9d75782febf185c63e5069bfc880543e0
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-07-08 17:42:58 -0300

    finished cronjob for starting bookings

--- util/cronjobs.js
+++ util/cronjobs.js
@@ -4,9 +4,12 @@ const {sendEmail} = require("../services/emailService");
 const Booking = require('../models/booking');
 const dayjs = require('dayjs');
 const {where} = require("sequelize");
+const {getUserById} = require("../services/userService");
+const User = require("../models/user");
+const Listing = require('../models/listing');
 
 exports.startingBookingsCronjob = async () => {
-    cron.schedule("* * * * * *", async () => {
+    cron.schedule("* * * * *", async () => {
         //"0 0 * * *" it's supposed to run every day at 00:00
         /*
         0 0 * * *
@@ -18,15 +21,26 @@ exports.startingBookingsCronjob = async () => {
         +----------- At 0 minute
         */
         try {
-            const now = dayjs();
-            let startingBookings = await Booking.findAll( {where:{id: 1}})
-            startingBookings.forEach( booking => {
-                    console.log(booking.start_date)
-                }
-            )
+            const now = dayjs().format('YYYY-MM-DDT03:00:00.000[Z]');
+            let startingBookings = await Booking.findAll( {where:{start_date: now}})
+            for (const booking of startingBookings) {
+                let client = await User.findByPk(booking.user_id);
+                let listing = await Listing.findByPk(booking.listing_id)
+                let owner = await User.findByPk(listing.user_id)
+                const ownerEmail = owner.email
+                const clientEmail = client.email
+                const emailSubject = 'A booking starts today'
+                const ownerMessage = `Your listing: '${listing.title}' is being booked today!`
+                const clientMessage = `Your booking for: '${listing.title}' starts today`
+
+                sendEmail(ownerEmail,emailSubject,ownerMessage)
+                sendEmail(clientEmail,emailSubject,clientMessage)
+            }
         } catch (error){
             console.log(error)
         }
 
     });
 }
+
+
